b7705844e4526118f8a03b618e851971f65ab79a
Eber <eberfelipearaujo@gmail.com>
2024-06-18 21:46:26 -0400

    Correcao de Codigo

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,5 +4,6 @@ const productController = require("../controllers/productController");
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.delete("/:id", authMiddleware, productController.deletarProduto);
+router.post("/", authMiddleware, productController.registerProduct);
 
 module.exports = router;
