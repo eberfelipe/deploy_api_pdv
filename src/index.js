a724b02f726be72bd616a9c047b8065a887446d3
Eber <eberfelipearaujo@gmail.com>
2024-07-09 02:14:39 -0400

    Swagger Configurado

--- src/index.js
+++ src/index.js
@@ -18,7 +18,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
-  explorer: true
+  swaggerOptions: {
+    authAction: {
+      ApiKeyAuth: {
+        name: "api_key",
+        schema: {
+          type: "apiKey",
+          in: "header",
+          name: "Authorization",
+          description: ""
+        },
+        value: process.env.SWAGGER_API_KEY
+      }
+    }
+  }
 }));
 
 app.use(express.json());
